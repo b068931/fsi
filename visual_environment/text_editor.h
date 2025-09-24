4dd8f0ede026edfa3ff0746fa1da25bd54a43782
b068931 <telefonchic41@gmail.com>
2025-09-24 18:09:08 +0300

    Refactor text editor into a separate class, make main window show up maximized.
    I also implemented some basic functionality for the text editor: now you can open and close files, the system checks whether the file is already open (if it is it just selects the tab), you can swap tabs.

---
+++ visual_environment/text_editor.h
@@ -0,0 +1,79 @@
+#ifndef VISUAL_ENVIRONMENT_TEXT_EDITOR_H
+#define VISUAL_ENVIRONMENT_TEXT_EDITOR_H
+
+#include <QTreeView>
+#include <QPair>
+#include <QTabWidget>
+#include <QString>
+#include <QVector>
+#include <QWidget>
+#include <QSplitter>
+
+class TextEditor : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit TextEditor(QWidget* parent = nullptr);
+
+    TextEditor(const TextEditor&) = delete;
+    TextEditor& operator=(const TextEditor&) = delete;
+
+    TextEditor(TextEditor&&) = delete;
+    TextEditor& operator=(TextEditor&&) = delete;
+
+    ~TextEditor() noexcept override;
+
+    void showEvent(QShowEvent* event) override;
+
+public:
+    /// <summary>
+    /// Opens the specified working directory.
+    /// </summary>
+    /// <param name="directoryPath">The path to the directory to open.</param>
+    void openWorkingDirectory(const QString& directoryPath);
+
+    /// <summary>
+    /// Retrieves the currently selected file and its selection status.
+    /// </summary>
+    /// <returns>A QPair containing the name of the currently selected file (QString) 
+    /// and a boolean indicating whether the file has been saved on disk.</returns>
+    QPair<QString, bool> getCurrentlySelectedFile() const;
+
+    /// <summary>
+    /// Sets the splitter ratio to its default value.
+    /// This makes the text editor larger than the working directory view.
+    /// The default ratio is approximately 1:6.
+    /// </summary>
+    void setDefaultSplitterRatio();
+
+private slots:
+    void onWorkingDirectoryItemDoubleClicked(const QModelIndex& index);
+    void onTabCloseRequested(int index);
+    void onTabMoved(int from, int to);
+
+private:
+    void openNewFile(const QString& filePath);
+    void closeFileAtIndex(int index);
+
+private:
+    // So that you can't accidentally change the layout of this widget
+    // from outside this class.
+    using QWidget::setLayout;
+
+    struct OpenedFile {
+        QString filePath;
+        bool isTemporary;
+    };
+
+    QVector<OpenedFile> openFiles;
+
+    QSplitter* splitter{};
+    QTabWidget* fileTabs{};
+    QTreeView* workingDirectory{};
+
+    void connectSignalsManually();
+    void setupEditorComponents();
+};
+
+#endif
