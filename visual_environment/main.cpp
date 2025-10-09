63b04f2c26e616ad4e61f28ea42bcdbcf14e2321
b068931 <telefonchic41@gmail.com>
2025-10-09 18:33:06 +0300

    Implement a simple custom style for the app.

--- visual_environment/main.cpp
+++ visual_environment/main.cpp
@@ -3,10 +3,20 @@
 #endif
 
 #include <QtWidgets/QApplication>
+#include <QFile>
+#include <QTextStream>
 #include "main_window.h"
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
+
+    // Load and apply stylesheet
+    QFile styleFile(":/styles/dark_theme.qss");
+    if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        QTextStream styleStream(&styleFile);
+        application.setStyleSheet(styleStream.readAll());
+        styleFile.close();
+    }
 
     Windows::MainWindow mainWindow;
     mainWindow.showMaximized();
